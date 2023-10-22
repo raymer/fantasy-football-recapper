ac4b68d12acc088f3ea7cf88341c980735ac88e7
Jonathan Raymer <rayme083@gmail.com>
2023-10-22 05:57:04 -0500

    week 8

--- Function/TimedRecapper/weekly_challenge.py
+++ Function/TimedRecapper/weekly_challenge.py
@@ -17,6 +17,8 @@ def getWeeklyWinner(league, week):
     return getWeek6Winner(league, week)
   elif week == 7:
     return getWeek7Winner(league, week)
+  elif week == 8:
+    return getWeek8Winner(league, week)
   return ""
 
 def getWeek2Winner(league, week):
@@ -192,5 +194,54 @@ def getWeek7Winner(league, week):
   message += f"\n\nHere's the full breakdown:\n"
   for team in sortedMarginsOfVictory:
     message += f"{team.teamName.strip()} who had a margin of victory of {str(round(team.marginOfVictory, 2))} points\n"
+  
+  return message
+
+
+def calculateRushYards(player):
+  playerDict = json.loads(str(player.stats).replace("{3:", "{\"3\":").replace("'", "\""))
+  if "breakdown" not in playerDict["3"]:
+    return 0
+  
+  breakdown = playerDict["3"]["breakdown"]
+
+  touchDowns = 0
+  if "passingTouchdowns" in breakdown:
+    touchDowns += breakdown["passingTouchdowns"]
+  if "rushingTouchdowns" in breakdown:
+    touchDowns += breakdown["rushingTouchdowns"]
+  if "receivingTouchdowns" in breakdown:
+    touchDowns += breakdown["receivingTouchdowns"]
+
+  return touchDowns
+
+def getWeek8Winner(league, week):
+  box_scores = league.box_scores(week)
+  teamRushYards = []
+  for box_score in box_scores:
+
+    numRushYards = 0
+    for player in list(filter(lambda x: x.slot_position != 'BE', box_score.home_lineup)):
+      numRushYards += calculateRushYards(player)
+    
+    teamRushYards.append(Object(teamName=box_score.home_team.team_name, rushYards=numRushYards))
+
+    numRushYards = 0
+    for player in list(filter(lambda x: x.slot_position != 'BE', box_score.away_lineup)):
+      numRushYards += calculateRushYards(player)
+    
+    teamRushYards.append(Object(teamName=box_score.away_team.team_name, rushYards=numRushYards))
+
+  sortedRushYards = sorted(teamRushYards, key=lambda item: item.rushYards, reverse=True)
+
+  message = "\n\nWeekly Challenge\nWeek 3: Endzone Celebration - The team that scores the most offensive touchdowns wins.\n"
+  if sortedRushYards[0].rushYards == sortedRushYards[1].rushYards:
+    message += f"Uh oh, there was a tie. Multiple teams scored {str(sortedRushYards[0].rushYards)} touchdowns"
+  else:
+    message += f"The winner was {sortedRushYards[0].teamName} who scored {str(sortedRushYards[0].rushYards)} touchdowns"
+
+  message += f"\n\nHere's the full breakdown:\n"
+  for team in sortedRushYards:
+    message += f"{team.teamName.strip()} {team.rushYards}\n"
   
   return message

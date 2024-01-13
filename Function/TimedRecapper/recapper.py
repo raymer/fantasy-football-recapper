fb0be4f512fac9809223c25b89586b20b0458cf2
Jonathan Raymer <rayme083@gmail.com>
2024-01-13 17:30:54 -0600

    updates

--- Function/TimedRecapper/recapper.py
+++ Function/TimedRecapper/recapper.py
@@ -4,16 +4,13 @@ from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 import logging
 from . import weekly_scores_summarizer
+import datetime
 
-def generateRecap(_swid, _espn_s2, _openAiKey, _slackToken, _slackChannelId, _leagueId):
-  swid = _swid
-  espn_s2 = _espn_s2
-  openai.api_key = _openAiKey
-  slackBotToken = _slackToken
-  slackChannelId = _slackChannelId
-  leagueId = _leagueId
+def generateRecap(swid, espn_s2, openAiKey, slackBotToken, slackChannelId, leagueId):
+  openai.api_key = openAiKey
 
-  league = League(league_id=leagueId, year=2023, espn_s2=espn_s2, swid=swid)
+  lastYear = datetime.date.today().year - 1
+  league = League(league_id=leagueId, year=lastYear, espn_s2=espn_s2, swid=swid)
 
   currentWeek = league.current_week if league.current_week == 1 else league.current_week - 1
 
@@ -21,17 +18,14 @@ def generateRecap(_swid, _espn_s2, _openAiKey, _slackToken, _slackChannelId, _le
 
   messages = [ {"role": "system", "content": "You are a intelligent assistant."} ]
   message = f"User : Pretending to be the commissioner of our fantasy football league, write a funny/condescending summary of this week's (week {currentWeek}) results in the fantasy football league given this data about the teams and scores: {weeklySummary}"
-
   messages.append({"role": "user", "content": message})
   chat = openai.ChatCompletion.create(model="gpt-4-1106-preview", messages=messages)
   chatGPTResponse = chat.choices[0].message.content
-  logging.info("Chat GPT Response Generated")
-  
+
   client = WebClient(token=slackBotToken)
   try:
     result = client.chat_postMessage(channel=slackChannelId, text=chatGPTResponse)
     logging.info("Posted to Slack")
-
   except SlackApiError as e:
     logging.info(f"Error: {e}")
 
